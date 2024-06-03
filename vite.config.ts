7c11ae26879cb32d891c04cb5f0a47d82dbb7170
nguyenhoangloc2208 <nguyenhoangloc2208@gmail.com>
2024-06-03 16:10:22 +0700

    reconfigure project structure

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,12 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    coverage: {
+      include: ['src/**/*'],
+    },
+    setupFiles: ['src/test/setup.ts'],
+  },
 })
