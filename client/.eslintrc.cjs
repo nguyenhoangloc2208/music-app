f0281bae5e66c37869409b7fe1f6b73e9c285771
nguyenhoangloc2208 <nguyenhoangloc2208@gmail.com>
2024-06-03 15:59:22 +0700

    configurations for project setup

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -5,14 +5,36 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],
   rules: {
+    'max-lines-per-function': [
+      'warning',
+      {
+        max: 100,
+        skipBlankLines: true,
+        skipComments: true,
+      },
+    ],
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    'react/jsx-sort-props': [
+      'error',
+      {
+        callbacksLast: true,
+        shorthandFirst: true,
+        shorthandLast: true,
+        multiline: 'last',
+        ignoreCase: true,
+        noSortAlphabetically: true,
+        reservedFirst: true,
+      },
+    ],
   },
-}
+};
